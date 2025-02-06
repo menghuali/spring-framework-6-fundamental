abe22114c38ab8de9ac3257e6d2b7e03518b4451
Menghua Li <menghua.li@westjet.com>
2025-02-06 08:52:30 -0500

    Finished XML configuration

--- conference/src/main/java/aloha/spring/Application.java
+++ conference/src/main/java/aloha/spring/Application.java
@@ -1,11 +1,15 @@
 package aloha.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import aloha.spring.service.SpeakerService;
-import aloha.spring.service.SpeakerServiceImpl;
 
 public class Application {
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appCtx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // SpeakerService service = new SpeakerServiceImpl();
+        SpeakerService service = appCtx.getBean("speakerService", SpeakerService.class);
         service.findAll().stream().forEach(speaker -> {
             System.out.println(
                     String.format("First name: %s, Last name: %s", speaker.getFirstName(), speaker.getLastName()));
--- conference/src/main/java/aloha/spring/service/SpeakerServiceImpl.java
+++ conference/src/main/java/aloha/spring/service/SpeakerServiceImpl.java
@@ -3,15 +3,29 @@ package aloha.spring.service;
 import java.util.List;
 
 import aloha.spring.model.Speaker;
-import aloha.spring.repo.StubSpeakerRepoImpl;
+import aloha.spring.repo.SpeakerRepo;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private StubSpeakerRepoImpl repo = new StubSpeakerRepoImpl();
+    private SpeakerRepo repo;
+
+    public SpeakerServiceImpl() {
+        System.out.println("SpeakerServiceImpl: no args constructor");
+    }
+
+    public SpeakerServiceImpl(SpeakerRepo repo) {
+        System.out.println("SpeakerServiceImpl: constructor injection");
+        this.repo = repo;
+    }
 
     @Override
     public List<Speaker> findAll() {
         return repo.findAll();
+    }
+
+    public void setRepo(SpeakerRepo repo) {
+        System.out.println("SpeakerServiceImpl: setter injection");
+        this.repo = repo;
     }
 
 }
